20b35eb50a9b7fc35b93ad6b1c0938a901793c93
Bruno Oliveira <bruno.oliveira@liferay.com>
2021-01-26 08:15:10 -0300

    Initialize repository with Spring Intializr

---
+++ backend/src/test/java/com/devsuperior/dsproject/DsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
