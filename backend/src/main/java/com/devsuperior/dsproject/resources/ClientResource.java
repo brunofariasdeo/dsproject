b89a1b5a3438bfceeb8d8b2cb94e15d6ec24122a
Bruno Oliveira <bruno.oliveira@liferay.com>
2021-01-26 09:43:33 -0300

    Make ClientResources collect info from service

--- backend/src/main/java/com/devsuperior/dsproject/resources/ClientResource.java
+++ backend/src/main/java/com/devsuperior/dsproject/resources/ClientResource.java
@@ -1,26 +1,26 @@
 package com.devsuperior.dsproject.resources;
 
-import java.time.Instant;
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsproject.entities.Client;
+import com.devsuperior.dsproject.services.ClientService;
 
 @RestController
 @RequestMapping(value = "/clients")
 public class ClientResource {
 	
+	@Autowired
+	private ClientService service;
+	
 	@GetMapping
 	public ResponseEntity<List<Client>> findAll(){
-		List<Client> list = new ArrayList<>();
-		Instant birthDate = Instant.parse("1994-09-18T00:00:00.63Z");
-		list.add(new Client(1L, "Bruno", "0123456789", 1600.00, birthDate, 0));
-		
+		List<Client> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 
 	}
