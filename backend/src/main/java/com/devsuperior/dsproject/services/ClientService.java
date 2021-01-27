1b834e232b260606707aad0639c41f27aef919c2
Bruno Oliveira <bruno.oliveira@liferay.com>
2021-01-27 09:07:32 -0300

    Create find client by id route

--- backend/src/main/java/com/devsuperior/dsproject/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/dsproject/services/ClientService.java
@@ -2,6 +2,7 @@ package com.devsuperior.dsproject.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsproject.dto.ClientDTO;
 import com.devsuperior.dsproject.entities.Client;
+import com.devsuperior.dsproject.exceptions.EntityNotFoundException;
 import com.devsuperior.dsproject.repositories.ClientRepository;
 
 @Service
@@ -27,5 +29,12 @@ public class ClientService {
 		}
 
 		return listDTO;
+	}
+	
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		return new ClientDTO(entity);
 	}
 }
